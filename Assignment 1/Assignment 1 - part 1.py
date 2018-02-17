32fc2c55f237691b163e9c26a97e0e97a6d93fae
Henkolicious <me@henriklarsson.eu>
2018-02-17 11:59:18 +0100

    Renamed variables and printet output in document

--- Assignment 1/Assignment 1 - part 1.py
+++ Assignment 1/Assignment 1 - part 1.py
@@ -8,20 +8,34 @@ import numpy as np
 def print_input(arg1, arg2, arg3):
     print('\nA: {}\nB: {}\nC: {}\n'.format(arg1, arg2, arg3))
 
-a = [2.5, 3.6, 1.2, 0.8, 4.0, 3.4]
-b = [1.2, 1.0, 1.8, 0.9, 3.0, 2.2]
-c = [8.0, 15.0, 12.0, 6.0, 8.0, 10.0]
+x1 = [2.5, 3.6, 1.2, 0.8, 4.0, 3.4]
+x2 = [1.2, 1.0, 1.8, 0.9, 3.0, 2.2]
+x3 = [8.0, 15.0, 12.0, 6.0, 8.0, 10.0]
 print("###### Input ######")
-print_input(a, b, c)
+print_input(x1, x2, x3)
 
-corrA_B = np.corrcoef(a, b)[1, 0]
-print("Correlation coefficient between A and B: ", corrA_B)
+corrX1_X2 = np.corrcoef(x1, x2)[1, 0]
+print("Correlation coefficient between A and B: ", corrX1_X2)
 
-corrA_C = np.corrcoef(a, c)[1, 0]
-print("Correlation coefficient between A and C: ", corrA_C)
+corrX1_X3 = np.corrcoef(x1, x3)[1, 0]
+print("Correlation coefficient between A and C: ", corrX1_X3)
 
-corrB_C = np.corrcoef(b, c)[1,0]
-print("Correlation coefficient between B and C: ", corrB_C)
+corrX2_X3 = np.corrcoef(x2, x3)[1,0]
+print("Correlation coefficient between B and C: ", corrX2_X3)
 
-correlation_table = np.corrcoef([a, b, c])
+correlation_table = np.corrcoef([x1, x2, x3])
 print("\nCorrelation coefficient between A, B and C\n\n", correlation_table, "\n")
+
+'''
+Output:
+
+Correlation coefficient between A and B:  0.5297480198963057
+Correlation coefficient between A and C:  0.3144432542848067
+Correlation coefficient between B and C:  -0.12945492014724624
+
+Correlation coefficient between A, B and C
+
+ [[ 1.          0.52974802  0.31444325]
+ [ 0.52974802  1.         -0.12945492]
+ [ 0.31444325 -0.12945492  1.        ]]
+'''
