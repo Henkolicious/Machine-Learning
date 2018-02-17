4f1bb7437c88d01264f7317d5ace0d9a78dde41c
Henkolicious <me@henriklarsson.eu>
2018-02-17 12:02:58 +0100

    Renamed output a,b,c --> x1, x2, x3

--- Assignment 1/Assignment 1 - part 1.py
+++ Assignment 1/Assignment 1 - part 1.py
@@ -6,7 +6,7 @@
 import numpy as np
 
 def print_input(arg1, arg2, arg3):
-    print('\nA: {}\nB: {}\nC: {}\n'.format(arg1, arg2, arg3))
+    print('X1: {}\nX2: {}\nX3: {}\n'.format(arg1, arg2, arg3))
 
 x1 = [2.5, 3.6, 1.2, 0.8, 4.0, 3.4]
 x2 = [1.2, 1.0, 1.8, 0.9, 3.0, 2.2]
@@ -15,25 +15,29 @@ print("###### Input ######")
 print_input(x1, x2, x3)
 
 corrX1_X2 = np.corrcoef(x1, x2)[1, 0]
-print("Correlation coefficient between A and B: ", corrX1_X2)
+print("Correlation coefficient between X1 and X2: ", corrX1_X2)
 
 corrX1_X3 = np.corrcoef(x1, x3)[1, 0]
-print("Correlation coefficient between A and C: ", corrX1_X3)
+print("Correlation coefficient between X1 and X3: ", corrX1_X3)
 
 corrX2_X3 = np.corrcoef(x2, x3)[1,0]
-print("Correlation coefficient between B and C: ", corrX2_X3)
+print("Correlation coefficient between X2 and X3: ", corrX2_X3)
 
 correlation_table = np.corrcoef([x1, x2, x3])
-print("\nCorrelation coefficient between A, B and C\n\n", correlation_table, "\n")
+print("\nCorrelation coefficient between X1, X2 and X3\n\n", correlation_table, "\n")
 
 '''
 Output:
+###### Input ######
+X1: [2.5, 3.6, 1.2, 0.8, 4.0, 3.4]
+X2: [1.2, 1.0, 1.8, 0.9, 3.0, 2.2]
+X3: [8.0, 15.0, 12.0, 6.0, 8.0, 10.0]
 
-Correlation coefficient between A and B:  0.5297480198963057
-Correlation coefficient between A and C:  0.3144432542848067
-Correlation coefficient between B and C:  -0.12945492014724624
+Correlation coefficient between X1 and X2:  0.5297480198963057
+Correlation coefficient between X1 and X3:  0.3144432542848067
+Correlation coefficient between X2 and X3:  -0.12945492014724624
 
-Correlation coefficient between A, B and C
+Correlation coefficient between X1, X2 and X3
 
  [[ 1.          0.52974802  0.31444325]
  [ 0.52974802  1.         -0.12945492]
