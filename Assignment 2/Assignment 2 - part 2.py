feadad23360b9ef35b01b744b699fe260f807d5c
Henkolicious <me@henriklarsson.eu>
2018-03-13 00:01:52 +0100

    part 2 start

--- Assignment 2/Assignment 2 - part 2.py
+++ Assignment 2/Assignment 2 - part 2.py
@@ -21,4 +21,79 @@ After that, apply K means clustering algorithm to the difference image to obtain
 changed pixels as 1 and unchanged pixels as 0.
 '''
 
-# use 11x11 so pad with 0
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
+
+from sklearn.cluster import KMeans
+
+
+def getDistanceMatrix(a, b):
+    distanceMatrix = []  # np.array([])
+
+    for i in range(a.shape[0]):
+        tmp = []
+        for j in range(a.shape[0]):
+            absoluteValue = np.abs(a[i][j] - b[i][j])
+            tmp.append(absoluteValue)
+
+        distanceMatrix.append(tmp)
+    return np.array(distanceMatrix)
+
+
+if __name__ == '__main__':
+
+    matrixA = np.array([
+        [154, 157, 157, 157, 150, 150, 170, 170, 175, 190],
+        [154, 157, 157, 151, 153, 155, 180, 180, 170, 190],
+        [154, 157, 150, 154, 160, 160, 160, 155, 155, 165],
+        [157, 157, 148, 148, 148, 160, 150, 155, 155, 165],
+        [100, 102, 104, 157, 142, 180, 170, 165, 10, 20],
+        [100, 103, 105, 165, 155, 180, 175, 162, 40, 50],
+        [100, 102, 108, 132, 180, 180, 172, 167, 25, 63],
+        [18, 28, 48, 12, 13, 20, 5, 15, 30, 40],
+        [15, 36, 46, 18, 21, 22, 28, 32, 30, 36],
+        [17, 21, 24, 26, 35, 45, 28, 30, 40, 20]
+    ])
+
+    matrixB = np.array([
+        [152, 156, 157, 156, 149, 150, 170, 160, 175, 190],
+        [154, 159, 157, 151, 153, 155, 180, 180, 170, 190],
+        [153, 157, 155, 154, 160, 160, 160, 155, 155, 165],
+        [157, 157, 148, 148, 148, 160, 150, 155, 155, 165],
+        [101, 102, 104, 159, 143, 180, 170, 165, 110, 220],
+        [99, 103, 105, 164, 155, 179, 175, 162, 240, 250],
+        [100, 102, 108, 132, 180, 180, 172, 167, 155, 163],
+        [118, 123, 148, 129, 109, 120, 155, 215, 140, 180],
+        [156, 136, 210, 218, 175, 122, 128, 232, 180, 156],
+        [178, 231, 245, 226, 215, 145, 188, 230, 170, 140],
+    ])
+
+    distanceMatrix = getDistanceMatrix(matrixA, matrixB)
+    # print(distanceMatrix)
+
+    # pad to get odd numbers, e.g 11x11
+    if len(distanceMatrix - 1) % 2 == 0:
+        paddedDistanceMatrix = np.pad(distanceMatrix, 0, mode='constant')
+
+    print(paddedDistanceMatrix)
+    # kmeansModel = KMeans()
+    # kmeansModel.fit(paddedDistanceMatrix)
+
+    # # degrees of variance - center plot in each cluster
+    # centroids = kmeansModel.cluster_centers_
+
+    # #plt.plot(centroids[:, 0], centroids[:, 1], marker='x', color='r')
+    # #plt.scatter(centroids[:, 0], centroids[:, 1],
+    # #            marker="x", s=150, linewidths=5, zorder=10)
+    # plt.plot(paddedDistanceMatrix)
+    # plt.show()
+
+    # for i in range(len(distanceMatrix)):
+    #     print("Cordinate: ", distanceMatrix[i], "Lable: ", lables[i])
+    #     plt.plot(distanceMatrix[i][0], distanceMatrix[i]
+    #              [1], colors[lables[i]], markersize=10)
+
+        # plt.scatter(centroids[:, 0], centroids[:, 1],
+        #             marker="x", s=150, linewidths=5, zorder=10)
